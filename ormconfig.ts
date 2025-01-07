0c6029942ecda0d13e042937ab82b2fb56474e1a
wilgnersoftlive <wilgner.gomes@softlive.dev>
2025-01-07 15:45:11 -0400

    feat: add orm config file

---
+++ ormconfig.ts
@@ -0,0 +1,17 @@
+import { DataSource } from 'typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+export default new DataSource({
+  type: 'mysql',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  entities: [__dirname + '/database/entities/*.entity{.ts,.js}'],
+  migrations: [__dirname + '/database/migrations/*{.ts,.js}'],
+  synchronize: false,
+  logging: true,
+});
